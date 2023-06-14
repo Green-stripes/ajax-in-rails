3ba4c5f339dc1dc3c912d18904a11cfc9f4a23af
Kaveri Bhattacharya <kavericb@hotmail.com>
2023-06-14 21:11:22 +0200

    project complete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,10 +4,14 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    if @review.save
-      redirect_to restaurant_path(@restaurant)
-    else
-      render 'restaurants/show', status: :unprocessable_entity
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to restaurant_path(@restaurant) }
+        format.json
+      else
+        format.html { render 'restaurants/show', status: :unprocessable_entity }
+        format.json
+      end
     end
   end
 
@@ -16,5 +20,4 @@ class ReviewsController < ApplicationController
   def review_params
     params.require(:review).permit(:content)
   end
-
 end
--- app/javascript/controllers/insert_in_list_controller.js
+++ app/javascript/controllers/insert_in_list_controller.js
@@ -2,6 +2,31 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="insert-in-list"
 export default class extends Controller {
+  static targets = ["form", "items"]
   connect() {
+    // console.log(this.element)
+    // console.log(this.formTarget)
+    // console.log(this.itemsTarget)
   }
-}
+
+  send(event) {
+    event.preventDefault()
+    // console.log(this.formTarget.action)
+    // const formData = new FormData(this.formTarget)
+    // console.log(formData)
+
+    fetch(this.formTarget.action, {
+      method: "POST",
+      headers: { "Accept": "application/json" },
+      body: new FormData(this.formTarget)
+    })
+      .then(response => response.json())
+      .then((data) => {
+        if (data.inserted_item) {
+          this.itemsTarget.insertAdjacentHTML("beforeend", data.inserted_item)
+        }
+        this.formTarget.outerHTML = data.form
+      })
+      }
+  }
+  
